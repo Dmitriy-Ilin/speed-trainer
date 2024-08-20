9d577db62a6fcbe98b22d838fda57d33223fd3f6
Dmitriy-Ilin <Ilin.Dima-1605982014@yandex.ru>
2024-08-20 13:37:39 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'speed-trainer',
+  base: 'typing-speed-trainer',
 });
 
